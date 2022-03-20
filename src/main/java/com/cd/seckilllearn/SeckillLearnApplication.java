c9f44f4ad896f9c4f5e117bd205c267df5f37820
CaiDi <1394180838@qq.com>
2022-03-20 15:39:45 +0800

    修改后提交

--- src/main/java/com/cd/seckilllearn/SeckillLearnApplication.java
+++ src/main/java/com/cd/seckilllearn/SeckillLearnApplication.java
@@ -8,6 +8,7 @@ public class SeckillLearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SeckillLearnApplication.class, args);
+        System.out.println("hello git");
     }
 
 }
