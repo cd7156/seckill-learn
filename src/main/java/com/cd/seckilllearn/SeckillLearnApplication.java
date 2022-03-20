a85409d53604e597624fee6654496407cad8f06f
CaiDi <1394180838@qq.com>
2022-03-20 15:31:52 +0800

    idea第一次提交

---
+++ src/main/java/com/cd/seckilllearn/SeckillLearnApplication.java
@@ -0,0 +1,13 @@
+package com.cd.seckilllearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillLearnApplication.class, args);
+    }
+
+}
